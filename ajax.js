7e637c11284e5ad5ed6e913c146ed70ab2d0da0a
henry6666 <aguinagahenry@gmail.com>
2016-05-02 11:45:36 +0200

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -9,9 +9,12 @@ Ajax är en teknik, en kombination av saker, för att hämta data från URLs
 Asyncronous Javascript and XML
 
 påbörja en hämtning
-var request = new XMLHttpRequest();
+var request = new XMLHttpRequest(req, res);
 request.onreadystatechange = function() {
   console.log("hej där")
+  
+  // I variabel res (response)  finns de information..
+  console.log("Svaret (response) är" + res.responseText);
   
 }
 request.open('GET', 'http://www.google.com');
