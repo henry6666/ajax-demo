365979ba5fada0a5815ee7a44a516055421aa64e
henry6666 <aguinagahenry@gmail.com>
2016-05-02 11:39:27 +0200

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,1 +1,18 @@
 javascript med ajax
+
+Vi ska använda ajaz för att hämta innehåll på en hemsida
+
+vad är Ajax?
+
+Ajax är en teknik, en kombination av saker, för att hämta data från URLs
+
+Asyncronous Javascript and XML
+
+påbörja en hämtning
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+  console.log("hej där")
+  
+}
+request.open('GET', 'http://www.google.com');
+request.send();
